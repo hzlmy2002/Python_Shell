cd8196c6ee6c20afc0bbdf6b55a6bdd51b15ccab
Prithvi Kohli <69433082+prithvikohli@users.noreply.github.com>
2021-11-08 01:41:41 +0000

    using parser combinators

--- src/parser.py
+++ src/parser.py
@@ -1,34 +1,32 @@
-import re
-from glob import glob
-from Stream import Stream
+from parsita import *
 
 
-class Parser:
-    def __init__(self) -> None:
-        pass
+class CommandParsers(TextParsers, whitespace=None):
+    single_quoted = "'" >> reg(r"[^\n\r']") << "'"
+    backquoted = "`" >> reg(r"[^\n\r`]") << "`"
+    double_quoted = (
+        '"' >> rep(backquoted | reg(r"[^\n\r\"`]")) << '"' > "".join
+    )  # concatenate list returned by rep() into single quote argument
+    quoted = single_quoted | backquoted | double_quoted
 
-    def setup_stream(self, tokens):
-        # Current setup includes the parameters into arguments
-        application = tokens[0]
-        arguments = tokens[1:]
-        return Stream(1, application, None, arguments, None)
+    unquoted = reg(r"[^\s'\"`;|<>]")
 
-    def parse_token(self, command):
-        # Separates command into a token (list [app,args]) and returns it
-        tokens = []
-        for m in re.finditer("[^\\s\"']+|\"([^\"]*)\"|'([^']*)'", command):
-            if m.group(1) or m.group(2):
-                quoted = m.group(0)
-                tokens.append(quoted[1:-1])
-            else:
-                globbing = glob(m.group(0))
-                if globbing:
-                    tokens.extend(globbing)
-                else:
-                    tokens.append(m.group(0))
-        return tokens
+    argument = quoted | unquoted > Argument
 
-    def parse(self, sing_com):
-        # Packs input singular command into an stream object
-        tokens = self.parse_token(sing_com)
-        return self.setup_stream(tokens)
+    whitespace = reg(r"[ \t]+")
+    redirection = (">" & whitespace >> argument) | (
+        "<" & whitespace >> argument
+    ) > Redirection
+
+    atom = redirection | argument
+
+    call = (
+        unquoted
+        > AppFactory << whitespace >> rep(redirection << whitespace)
+        & argument
+        & rep(whitespace >> atom)
+    ) > Call
+
+    command = fwd()
+    seq = command << ";" >> command
+    command.define(call | seq)
