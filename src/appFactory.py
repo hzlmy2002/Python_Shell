9246216cdcb9e8d6745100ab349abf08fc391406
Prithvi Kohli <69433082+prithvikohli@users.noreply.github.com>
2021-11-29 13:15:04 +0000

    removed app checking from parser

--- src/appFactory.py
+++ src/appFactory.py
@@ -5,33 +5,25 @@ class AppNotFoundError(RuntimeError):
     pass
 
 
-class appFactory:
-    def __init__(self):
-        self.appMap = {}
-        self.appMap["pwd"] = Pwd()
-        self.appMap["echo"] = Echo()
-        self.appMap["cd"] = Cd()
-        self.appMap["cat"] = Cat()
-        self.appMap["_cat"] = CatUnsafe()
-        self.appMap["ls"] = Ls()
-        self.appMap["head"] = Head()
-        self.appMap["tail"] = Tail()
-        self.appMap["grep"] = Grep()
-        self.appMap["cut"] = Cut()
-        self.appMap["find"] = Find()
-        self.appMap["uniq"] = Uniq()
-        self.appMap["sort"] = Sort()
-
-    def get(self, app_name):
-        if app_name in self.appMap:
-            return self.appMap[app_name]
-        else:
-            raise AppNotFoundError(app_name)
+appMap = {}
+appMap["pwd"] = Pwd()
+appMap["echo"] = Echo()
+appMap["cd"] = Cd()
+appMap["cat"] = Cat()
+appMap["_cat"] = CatUnsafe()
+appMap["ls"] = Ls()
+appMap["head"] = Head()
+appMap["tail"] = Tail()
+appMap["grep"] = Grep()
+appMap["cut"] = Cut()
+appMap["find"] = Find()
+appMap["uniq"] = Uniq()
+appMap["sort"] = Sort()
 
 
-def test():
-    af = appFactory().get("pwd")
-    pass
-
-
-test()
+def getApp(app_name):
+    try:
+        app = appMap[app_name]
+    except KeyError:
+        raise AppNotFoundError("Application not found.")
+    return app
