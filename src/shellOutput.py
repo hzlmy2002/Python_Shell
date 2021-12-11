0e6ba3d9b677eaf0fa6fe9f7f80b9973fb65d506
Minyi Lei <hzlmy001@gmail.com>
2021-12-11 23:48:14 +0000

    fixed violation of parser and shelloutput

--- src/shellOutput.py
+++ src/shellOutput.py
@@ -1,65 +1,66 @@
 from enum import Enum
-from apps import Stream
+
 
 class stdout(Enum):
     """
     Enum for the stdout of the shell
     """
-    std="standard"
-    pipe="pipe"
-    subs="substitution"
-    redir="redirection" # one off type, reset after write
+    std = "standard"
+    pipe = "pipe"
+    subs = "substitution"
+    redir = "redirection"  # one off type, reset after write
 
     def __str__(self):
         return self.value
 
+
 class ShellOutput:
-    def __init__(self,globalStream) -> None:
-        self.stream=globalStream
-        self.mode=stdout.std
+    def __init__(self, globalStream) -> None:
+        self.stream = globalStream
+        self.mode = stdout.std
 
-        self.isSubstitution=False
-        self.record=""
+        self.isSubstitution = False
+        self.record = ""
 
-        self.sandbox=[]
-        self.redirFileName=""
+        self.sandbox = []
+        self.redirFileName = ""
 
     def reset(self):
-        self.mode=stdout.std
+        self.mode = stdout.std
         self.sandbox.clear()
-        self.redirFileName=""
+        self.redirFileName = ""
 
     def cleanBuffer(self):
         self.sandbox.clear()
 
-    def write(self,content):
+    def write(self, content):
         # Write redirection will be automatically reset afterwards
-        if self.mode==stdout.std:
+        if self.mode == stdout.std:
             if self.isSubstitution:
-                self.record+=content
+                self.record += content
             else:
-                print(content,end="")
+                print(content, end="")
 
-        elif self.mode==stdout.pipe:
-            if len(self.sandbox)==0:
+        elif self.mode == stdout.pipe:
+            if len(self.sandbox) == 0:
                 self.sandbox.append(content)
             else:
                 self.cleanBuffer()
                 self.sandbox.append(content)
-        elif self.mode==stdout.redir:
-            with open(self.redirFileName,"w") as f:
+        elif self.mode == stdout.redir:
+            with open(self.redirFileName, "w") as f:
                 f.write(content)
             self.reset()
         else:
             raise Exception("Unknown output mode")
-    
-    def getBuffer(self,reset=False):
-        if self.mode==stdout.pipe:
-            output=""
-            if len(self.sandbox)==0:
+
+    def getBuffer(self, reset=False):
+        if self.mode == stdout.pipe:
+            output = ""
+            if len(self.sandbox) == 0:
                 return output
             else:
-                output=self.sandbox[0]
+                output = self.sandbox[0]
             if reset:
                 self.reset()
             return output
@@ -69,15 +70,15 @@ class ShellOutput:
     def getSubstitutedRecord(self):
         return self.record
 
-    def setMode(self,mode):
+    def setMode(self, mode):
         self.reset()
-        if mode==stdout.subs:
-            self.isSubstitution=True
+        if mode == stdout.subs:
+            self.isSubstitution = True
         else:
-            self.mode=mode
+            self.mode = mode
 
-    def setRedirFileName(self,filename):
-        self.redirFileName=filename
+    def setRedirFileName(self, filename):
+        self.redirFileName = filename
 
     def getMode(self):
         return self.mode
