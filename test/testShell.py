c1c1a6448a9ddb313c4c9c7bbf74a0aed012443b
Prithvi Kohli <69433082+prithvikohli@users.noreply.github.com>
2021-12-20 13:21:55 +0000

    fixed formatting

--- test/testShell.py
+++ test/testShell.py
@@ -5,11 +5,13 @@ sys.path.insert(0, "../src")
 import unittest
 from shell import Shell
 
+
 class TestShell(unittest.TestCase):
     def testShell(self):
-        sh=Shell(os.getcwd())
-        sh.eval("   ",sys.stdout)
-        self.assertEqual(sh.getWorkingDir(),os.getcwd())
+        sh = Shell(os.getcwd())
+        sh.eval("   ", sys.stdout)
+        self.assertEqual(sh.getWorkingDir(), os.getcwd())
+
 
 if __name__ == "__main__":
     unittest.main()
